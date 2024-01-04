1b5db81d847a0563f8b528d5bfbf3b70c831a05c
tripathisatwik <satwiktripathi99@gmail.com>
2024-01-04 13:29:46 +0545

    day_9 added

---
+++ day_9/js/script.js
@@ -0,0 +1,29 @@
+//DOM - Element 
+//getElementById() - we can access HTML element by attribute
+//i.e. id
+//example
+const title = document.getElementById("title")
+const content = document.getElementById("content")
+const button = document.getElementById("button")
+
+//changes
+title.innerHTML="HTML DOM- JS"
+content.innerHTML="JS DOM | HTML Element property"
+//here innerHTML property is use to manipulate data inside
+//HTML element
+//style
+button.style.color="WHITE"
+button.style.fontFamily="open-sans"
+button.style.backgroundColor="black"
+button.style.border="0"
+button.style.cursor="pointer"
+button.style.padding="5px"
+button.style.boxShadow="2px"
+button.style.borderRadius="50px"
+button.style
+
+//classlist property
+//add() and remove() method
+button.classList.add("button")
+
+//remove() method removes class of element
